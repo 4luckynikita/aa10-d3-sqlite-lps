8eb3aafe0ec204b7e24dc9e77f1a4c6b7fb0f813
Riley Bona <rileybona7@gmail.com>
2024-03-13 17:46:56 -0400

    most of phase 3

--- aa10-sql-basics-full-time/sql-practice/phase-3.sql
+++ aa10-sql-basics-full-time/sql-practice/phase-3.sql
@@ -1,1 +1,27 @@
--- Your code here 
+-- Your code here
+INSERT INTO customers (name, phone)
+VALUES ('Rachel', "111-111-111");
+
+-- check current points of rachel
+SELECT points
+FROM customers
+WHERE name='Rachel';
+-- update rachels points
+UPDATE customers
+SET points=6
+WHERE name='Rachel';
+
+-- then add coffee order (to coffee orders)
+INSERT INTO coffee_orders
+DEFAULT VALUES;
+
+-- testing coffee order
+SELECT * FROM coffee_orders
+DELETE FROM coffee_orders
+
+-- two new custies
+INSERT INTO customers (name, email, phone)
+VALUES ('Monica', "monica@friends.show", "222-222-2222");
+
+INSERT INTO customers (name, email, phone)
+VALUES ('Phoebe', "phoebe@friends.show", "333-333-3333");
